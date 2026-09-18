11ac4225753a03719da596f422a781ffedc7f757
johnpark236-tech <johnpark236@gmail.com>
2026-09-18 19:09:20 +0900

    Add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.BOARD_CONFIG = {
+  boardTitle: "천안시민AI강사양성",
+  apiUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  categories: ["교안_참고자료","프로그램","실습자료","실습1-교육계획안","실습2-인포그래픽","실습3-아이스브레이킹","실습4-수노음악"],
+  maxUploadMB: 5
+};
