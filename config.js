5d71c84fd7f5962110d73526fd5b051564709716
johnpark236-tech <johnpark236@gmail.com>
2026-09-18 19:49:09 +0900

    Connect Google Apps Script web app

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BOARD_CONFIG = {
   boardTitle: "AI 천안 만날 학습 동아리 게시판",
-  apiUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyCzexMVfd7rbwNjLjR0h4MKnmASLd34cEvYuTDVTSJpLDOP2ax-cWPSWIFLa4AXU14/exec",
   categories: ["교안_참고자료","프로그램","실습자료","실습1-교육계획안","실습2-인포그래픽","실습3-아이스브레이킹","실습4-수노음악"],
   maxUploadMB: 5
 };
