2ca73e7359f248b0228131cc137e8dc6f7d502a5
johnpark236-tech <johnpark236@gmail.com>
2026-09-18 19:14:35 +0900

    Rename board to AI 천안 만날 학습 동아리 게시판

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BOARD_CONFIG = {
-  boardTitle: "천안시민AI강사양성",
+  boardTitle: "AI 천안 만날 학습 동아리 게시판",
   apiUrl: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
   categories: ["교안_참고자료","프로그램","실습자료","실습1-교육계획안","실습2-인포그래픽","실습3-아이스브레이킹","실습4-수노음악"],
   maxUploadMB: 5
